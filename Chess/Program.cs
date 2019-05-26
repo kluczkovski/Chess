4ec40ba3566daf6f25b27173314c8b50982bdbef
Everton Kluczkovski <kluczkovski@hotmail.com>
2019-05-26 09:12:55 +0100

    TEste

---
+++ Chess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste");
+        }
+    }
+}
