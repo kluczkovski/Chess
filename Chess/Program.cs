64849b2428802664ae199d41f31544050a5302eb
Everton Kluczkovski <kluczkovski@hotmail.com>
2019-05-26 13:11:43 +0100

    Melhorando a impressão do Tabuleiro.

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -9,25 +9,29 @@ namespace Chess
         static void Main(string[] args)
         {
 
-            //try
-            //{
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.ColocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPecas(new Rei(tab, Cor.Preta), new Posicao(0, 1));
 
-            //    Tela.ImprimirTabuleiro(tab);
 
-            //}
-            //catch (TabuleiroException e)
-            //{
-            //    Console.WriteLine("Erro:" + e.Message);
+                tab.ColocarPecas(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-            //}
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+                Tela.ImprimirTabuleiro(tab);
+
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine("Erro:" + e.Message);
+
+            }
+
+            //PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            //Console.WriteLine(pos);
+            //Console.WriteLine(pos.toPosicao());
 
 
         }
--- Chess/Tela.cs
+++ Chess/Tela.cs
@@ -9,6 +9,7 @@ namespace Chess
         {
             for (int i=0; i<tab.Linhas; i++)
             {
+                Console.Write(8-i + " ");
                 for (int j = 0; j < tab.Linhas; j++)
                 {
                     if (tab.peca(i,j) == null)
@@ -16,11 +17,31 @@ namespace Chess
                         Console.Write("- ");
                     }else
                     {
-                        Console.Write(tab.peca(i, j) +  " ");
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
+
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
+
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.BackgroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
+
     }
 }
